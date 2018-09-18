217a9bab8ad21c79a704698734c4855379e695b4
DavidWood <david.wood@david-wood.local>
2018-09-18 14:47:43 -0400

    SubstringDemo

---
+++ SubstringDemo.py
@@ -0,0 +1,15 @@
+fName = input ("What is your first name?")
+lName = input ("What is your last nanme?")
+
+
+result = fName[0] + "." + lName 
+
+lenfName = len(fName)
+lenlName = len(lName)
+
+codeName = fName[0] + fName[-1] + "." + lName[0] + lName[-1]
+#codeName = fName[0] + fName[len(fName)-1]
+
+print ("Hi " + result)
+
+print ("your code name is:" + codeName)
